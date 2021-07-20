87206b8422495456d21f81116ab40050052c2a13
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-07-20 19:05:21 -0300

    import dotenv no mongoconnection

--- models/mongoConnection.js
+++ models/mongoConnection.js
@@ -1,11 +1,8 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.ATLAS_MONGO_URI;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("projects").collection("heroes");
-  client.close();
-});
 
 module.exports = client;
