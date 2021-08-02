faff3f3379b277e1d47cbfd1b967102c0ccfc672
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-08-02 14:44:12 -0300

    Adicionando a propriedade type ao erro JWT

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -6,12 +6,18 @@ const secret = process.env.JWT_SECRET;
 const validateJWT = (req, res, next) => {
   const { headers: { authorization } } = req;
   if (!authorization) {
-    return res.status(401).json({ message: 'Token not found' });
+    return res.status(401).json({
+      type: 'invalid_token',
+      message: 'Token not found',
+    });
   }
   try {
     const payload = jwt.verify(authorization, secret);
     if (!payload) {
-      return res.status(401).json({ message: 'Expired or invalid token' });
+      return res.status(401).json({
+        type: 'invalid_token',
+        message: 'Expired or invalid token',
+      });
     }
     req.payload = payload;
     next();
